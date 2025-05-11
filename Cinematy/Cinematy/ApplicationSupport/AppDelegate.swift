dafe78c771880008ff1ab58f98667b784f23d7f5
mohamedyoussef192 <49980714+m7mdyoussef@users.noreply.github.com>
2025-05-11 19:06:57 +0300

    setup project

---
+++ Cinematy/Cinematy/ApplicationSupport/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Cinematy
+//
+//  Created by Mohamed Youssef Al-Azizy on 11/05/2025.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
